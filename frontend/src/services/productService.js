219f319b164faac359804137528975c3696d7f66
Mohit Kumar <86540460+mohitchhapola@users.noreply.github.com>
2025-02-05 16:13:25 +0530

    Update productService.js

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://poultrybackend.vercel.app/";
+const BACKEND_URL = "https://poultrybackend.vercel.app/" ||  process.env.REACT_APP_BACKEND_URL;
 
 const API_URL = `${BACKEND_URL}/api/dailyAct/`;
 
